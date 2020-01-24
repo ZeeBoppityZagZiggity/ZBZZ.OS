f778647301695bba4d29ce323a2cb9d571657007
Charlie <chazzo96@yahoo.com>
2020-01-24 11:25:22 -0500

    Slight tweak before branching off to make uart object oriented.

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,17 @@
 const uart = @import("uart.zig");
+const UART_BASE_ADDR: usize = 0x10000000;
 
 // export const uart_base_addr = 0x10000000;
 
 export fn kinit() void {
   const x = 0;
-  // while(true) {}
-  uart.uart_init(0x10000000);
+  
+  uart.uart_init(UART_BASE_ADDR);
   var rx: ?u8 = null; 
   while(true) {
-    rx = uart.uart_read(0x10000000);
+    rx = uart.uart_read(UART_BASE_ADDR);
     if (rx != null) {
-        uart.uart_put(0x10000000, rx.?); 
+        uart.uart_put(UART_BASE_ADDR, rx.?); 
     }
   } //stay in zig for now
 }
